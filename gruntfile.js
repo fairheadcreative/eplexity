44818cc9c4cdd759fdd674ed1441505a263724b2
dcerimagic <dcerimagic@gmail.com>
2015-12-21 15:24:40 +0100

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {   
+      dist: {
+        src: [
+          'js/libs/jquery.min.js',
+          'js/layout/*.js'
+        ],
+        dest: 'js/production.js',
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'js/production.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    } 
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['concat', 'uglify']);
+};
